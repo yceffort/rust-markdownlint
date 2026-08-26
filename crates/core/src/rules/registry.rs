bf5c3b65e3ed5d9ca8904ef29b8bcde22ad15380
yceffort <yceffort@gmail.com>
2026-08-26 15:42:39 +0900

    feat(core): port MD011 no-reversed-links

--- crates/core/src/rules/registry.rs
+++ crates/core/src/rules/registry.rs
@@ -9,6 +9,7 @@ use super::md005::Md005;
 use super::md007::Md007;
 use super::md009::Md009;
 use super::md010::Md010;
+use super::md011::Md011;
 use super::md012::Md012;
 use super::md013::Md013;
 use super::md014::Md014;
@@ -37,10 +38,10 @@ use super::md043::Md043;
 use super::md046::Md046;
 use super::md047::Md047;
 
-static RULES: [&dyn Rule; 34] = [
-    &Md001, &Md003, &Md004, &Md005, &Md007, &Md009, &Md010, &Md012, &Md013, &Md014, &Md018, &Md019,
-    &Md020, &Md021, &Md022, &Md023, &Md024, &Md025, &Md026, &Md027, &Md028, &Md029, &Md030, &Md031,
-    &Md033, &Md034, &Md035, &Md036, &Md038, &Md040, &Md041, &Md043, &Md046, &Md047,
+static RULES: [&dyn Rule; 35] = [
+    &Md001, &Md003, &Md004, &Md005, &Md007, &Md009, &Md010, &Md011, &Md012, &Md013, &Md014, &Md018,
+    &Md019, &Md020, &Md021, &Md022, &Md023, &Md024, &Md025, &Md026, &Md027, &Md028, &Md029, &Md030,
+    &Md031, &Md033, &Md034, &Md035, &Md036, &Md038, &Md040, &Md041, &Md043, &Md046, &Md047,
 ];
 
 pub fn all_rules() -> &'static [&'static dyn Rule] {
