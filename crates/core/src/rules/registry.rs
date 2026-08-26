5d6ce74b1aeb3440c0eca8600e7e367e9fa4ba87
yceffort <yceffort@gmail.com>
2026-08-26 17:43:12 +0900

    feat(core): port MD053 link-image-reference-definitions

--- crates/core/src/rules/registry.rs
+++ crates/core/src/rules/registry.rs
@@ -48,12 +48,13 @@ use super::md049::Md049;
 use super::md050::Md050;
 use super::md051::Md051;
 use super::md052::Md052;
+use super::md053::Md053;
 
-static RULES: [&dyn Rule; 46] = [
+static RULES: [&dyn Rule; 47] = [
     &Md001, &Md003, &Md004, &Md005, &Md007, &Md009, &Md010, &Md011, &Md012, &Md013, &Md014, &Md018,
     &Md019, &Md020, &Md021, &Md022, &Md023, &Md024, &Md025, &Md026, &Md027, &Md028, &Md029, &Md030,
     &Md031, &Md032, &Md033, &Md034, &Md035, &Md036, &Md037, &Md038, &Md039, &Md040, &Md041, &Md042,
-    &Md043, &Md044, &Md045, &Md046, &Md047, &Md048, &Md049, &Md050, &Md051, &Md052,
+    &Md043, &Md044, &Md045, &Md046, &Md047, &Md048, &Md049, &Md050, &Md051, &Md052, &Md053,
 ];
 
 pub fn all_rules() -> &'static [&'static dyn Rule] {
