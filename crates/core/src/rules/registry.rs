1c0dc6dbb3fb0bdb7e30752ff463dc00cdc881d0
yceffort <yceffort@gmail.com>
2026-08-26 15:39:03 +0900

    feat(core): port MD046 code-block-style

--- crates/core/src/rules/registry.rs
+++ crates/core/src/rules/registry.rs
@@ -27,12 +27,13 @@ use super::md030::Md030;
 use super::md035::Md035;
 use super::md041::Md041;
 use super::md043::Md043;
+use super::md046::Md046;
 use super::md047::Md047;
 
-static RULES: [&dyn Rule; 26] = [
+static RULES: [&dyn Rule; 27] = [
     &Md001, &Md003, &Md004, &Md005, &Md007, &Md009, &Md010, &Md012, &Md013, &Md018, &Md019, &Md020,
     &Md021, &Md022, &Md023, &Md024, &Md025, &Md026, &Md027, &Md028, &Md029, &Md030, &Md035, &Md041,
-    &Md043, &Md047,
+    &Md043, &Md046, &Md047,
 ];
 
 pub fn all_rules() -> &'static [&'static dyn Rule] {
