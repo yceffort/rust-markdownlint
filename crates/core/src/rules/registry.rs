f3f34d0761ca64dfbe2dcb544d8d7cae43758737
yceffort <yceffort@gmail.com>
2026-08-26 14:46:33 +0900

    feat(core): port MD043 required-headings

--- crates/core/src/rules/registry.rs
+++ crates/core/src/rules/registry.rs
@@ -18,11 +18,12 @@ use super::md022::Md022;
 use super::md023::Md023;
 use super::md025::Md025;
 use super::md035::Md035;
+use super::md043::Md043;
 use super::md047::Md047;
 
-static RULES: [&dyn Rule; 17] = [
+static RULES: [&dyn Rule; 18] = [
     &Md001, &Md003, &Md004, &Md005, &Md007, &Md009, &Md010, &Md012, &Md018, &Md019, &Md020, &Md021,
-    &Md022, &Md023, &Md025, &Md035, &Md047,
+    &Md022, &Md023, &Md025, &Md035, &Md043, &Md047,
 ];
 
 pub fn all_rules() -> &'static [&'static dyn Rule] {
